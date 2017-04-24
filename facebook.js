adf2bfa46175d90aba61c801f20f46cb121c893e
Gaetan Leandre <gaetan.leandre@epitech.eu>
2017-04-24 11:32:40 +0200

    facebook version

--- facebook.js
+++ facebook.js
@@ -1,18 +1,18 @@
 //
-// twitter.js for noLitics in /Users/leandr_g/Documents/perso/nolitics/noLitics/
+// facebook.js for nolitics in /Users/leandr_g/Documents/perso/nolitics/noLitics/
 //
 // Made by Gaëtan Léandre
 // Login   <leandr_g@epitech.eu>
 //
-// Started on  Mon Apr 24 10:10:03 2017 Gaëtan Léandre
-// Last update Mon Apr 24 10:31:36 2017 Gaëtan Léandre
+// Started on  Mon Apr 24 10:34:58 2017 Gaëtan Léandre
+// Last update Mon Apr 24 11:30:04 2017 Gaëtan Léandre
 //
 
 var addImage = function(found)
 {
     var gif;
 
-    makeRequest("GET", "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=sexy").then(function(responseText){
+    makeRequest("GET", "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=funny").then(function(responseText){
         result = JSON.parse(responseText).data.image_url;
         if (!(result.includes("https")))
             result = result.replace("http", "https");
@@ -25,22 +25,29 @@ var addImage = function(found)
 
 var remplace = function()
 {
-    var aTags = document.getElementsByClassName("tweet");
+    var aTags = document.getElementsByClassName("fbUserContent _5pcr");
     var found;
     var img;
     var txt;
     var card;
+    var tmp;
 
     for (var i = 0; i < aTags.length; i++) {
       if (aTags[i] && !(aTags[i].classList.contains("noLitics")) && isIn(removeDiacritics(aTags[i].textContent.toUpperCase()))) {
-        found = aTags[i].getElementsByClassName("content")[0];
+        found = aTags[i];
+        if ((tmp = found.getElementsByClassName("fbUserContent")[0]) != undefined)
+        {
+            tmp.className += " noLitics";
+            found = tmp;
+        }
+        found = found.getElementsByClassName("_1dwg _1w_m")[0];
+        if (found)
+            found = found.lastChild;
         if (found)
         {
             addImage(found);
-            txt = found.getElementsByClassName("js-tweet-text-container")[0];
-            img = found.getElementsByClassName("AdaptiveMediaOuterContainer")[0];
-            card = found.getElementsByClassName("AdaptiveMediaOuterContainer")[0];
-
+            txt = found.getElementsByClassName("_5pbx userContent")[0];
+            img = found.getElementsByClassName("_3x-2")[0];
             if (txt)
                 found.removeChild(txt);
             if (img)
