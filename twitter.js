81296bb5c5d2a147463768f1406526715a374a97
Gaetan Leandre <gaetan.leandre@epitech.eu>
2017-04-24 12:18:55 +0200

    handle cards twitter

--- twitter.js
+++ twitter.js
@@ -5,7 +5,7 @@
 // Login   <leandr_g@epitech.eu>
 //
 // Started on  Mon Apr 24 10:10:03 2017 Gaëtan Léandre
-// Last update Mon Apr 24 11:32:50 2017 Gaëtan Léandre
+// Last update Mon Apr 24 12:17:40 2017 Gaëtan Léandre
 //
 
 var addImage = function(found)
@@ -29,6 +29,7 @@ var remplace = function()
     var found;
     var img;
     var txt;
+    var card;
 
     for (var i = 0; i < aTags.length; i++) {
       if (aTags[i] && !(aTags[i].classList.contains("noLitics")) && isIn(removeDiacritics(aTags[i].textContent.toUpperCase()))) {
@@ -38,11 +39,13 @@ var remplace = function()
             addImage(found);
             txt = found.getElementsByClassName("js-tweet-text-container")[0];
             img = found.getElementsByClassName("AdaptiveMediaOuterContainer")[0];
-
+            card = found.getElementsByClassName("card2")[0];
             if (txt)
                 found.removeChild(txt);
             if (img)
                 found.removeChild(img);
+            if (card)
+                found.removeChild(card);
         }
         aTags[i].className += " noLitics";
       }
